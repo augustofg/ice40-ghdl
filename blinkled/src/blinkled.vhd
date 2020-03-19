069adc9ca64038aab19328600f6963020217a325
Augusto Fraga Giachero <augustofg96@gmail.com>
2020-03-19 08:19:10 -0300

    Put the clk_div constant inside a generic declaration [blinkled]
    When instantiating the blinkled entity, it should be possible to
    change the led bliking period.

--- blinkled/src/blinkled.vhd
+++ blinkled/src/blinkled.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity blinkled is
+  generic (clk_div: integer := 5_999_999);
   port (
     clk: in std_logic;
     led0, led1, led2, led3, led4, led5, led6, led7: out std_logic
@@ -19,7 +20,7 @@ begin
     variable counter: unsigned (0 to 23) := x"000000";
   begin
     if rising_edge(clk) then
-      if counter = 5_999_999 then
+      if counter = clk_div then
         counter := x"000000";
         clk_2hz <= '1';
       else
